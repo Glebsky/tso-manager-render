2ec8b352c1bfe52242379486f197fc9acd54b3b6
hlib <h.kosianov@urancompany.com>
2026-09-04 15:45:36 +0300

    ✨ (feat): add dynamic title & new favicon and logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('ui.auth.login_title') }} — TSO Manager</title>
+    <title>{{ __('ui.auth.login_title') }} · TSO Manager</title>
 
     <!-- Theme & Color Scheme -->
     <meta name="theme-color" content="#020617">
